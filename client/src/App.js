6e0d4fe7ec83ea91f351df556df5cd25aa9113f6
Rishabh Raj <rishabhraj13951@gmail.com>
2024-06-07 16:05:44 +0530

    login and logout using OAuth

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,36 @@
 import Main from "./pages/Main";
+import Signup from "./components/Signup";
+import Logout from "./components/logout";
+import { useEffect } from "react";
+import { gapi } from "gapi-script"
+import { Route, Routes } from "react-router-dom"
+
+const clientId = "988563869606-he9454qn5qus04sq86hh0f9nmm955ba2.apps.googleusercontent.com"
 
 function App() {
+
+  useEffect(() => {
+    function start() {
+      gapi.client.init({
+        clientId: clientId,
+        scope: ""
+      })
+    };
+
+    gapi.load('client:auth2', start)
+  })
+
   return (
-    <Main />
+    <>
+      <Routes>
+        <Route exact path="/" element={<Signup />} />
+        <Route exact path="/mail" element={<Main />} />
+        <Route exact path="/logout" element={<Logout />} />
+      </Routes>
+      {/* <Signup /> */}
+      {/* <Logout /> */}
+      {/* <Main /> */}
+    </>
   );
 }
 
